4813f13e65066406830e1c6eb1fa32a502216ba8
Marcos-Henrique <marcoshenrique.dev27@gmail.com>
2026-03-07 22:09:06 -0300

    Deu erro mais amanhã eu corrijo
    deu pau na versão do prisma mais amanha eu volto e corrijo rapidinho pq ja sei oque é

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client/extension'
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(express.json())
 
 const users = []
 
 app.post('/users', (req, res)=> {
-    
+
     users.push(req.body)
 
-    res.send('Ok, post deu certin.')
+    res.status(201).json(req.body)
 })
 
 app.get('/users', (req, res) => {
-    res.json(users)
+    res.status(200).json(users)
 })
 
 app.listen(3000)
