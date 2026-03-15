94645ced0999c1c09e60fcd142f7d29b6fcd5079
Marcos-Henrique <marcoshenrique.dev27@gmail.com>
2026-03-15 15:49:31 -0300

    Changed Message

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.delete("/users/:id", async (req, res) => {
     },
   });
 
-  res.status(200).json({ message: "Usuário Deletado com sucesso" });
+  res.status(200).json({ message: "User successfully deleted" });
 });
 
 app.listen(3000)
