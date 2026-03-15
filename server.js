6f4c953afce1abd72532aecabcdab0ff65e2b52f
Marcos-Henrique <marcoshenrique.dev27@gmail.com>
2026-03-15 00:02:46 -0300

    Axios install

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
+
 
 const app = express()
 const prisma = new PrismaClient()
 
 app.use(express.json())
-
+app.use(cors())
 
 
 app.post('/users', async (req, res)=> {
